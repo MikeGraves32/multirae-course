472c6b5e4660fa5ab27d9e00ddeba6903dc0b773
MikeGraves32 <93281224+MikeGraves32@users.noreply.github.com>
2022-09-12 23:13:10 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,38 +15,40 @@ import Container from "react-bootstrap/Container";
 // import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-return (
-     <DndProvider backend={HTML5Backend}>
-          <div className='App container-md'>
-               <div className='row'>
-                    <h1>Welcome to the MultiRae Course</h1>
-                    <LoremIpsum p={1} />
-                    <br />
-                    <Link to='/pages/LandingPage'>Landing Page</Link>
-               </div>
+function App() {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div className="App container-md">
+        <div className="row">
+          <h1>Welcome to the MultiRae Course</h1>
+          <LoremIpsum p={1} />
+          <br />
+          <Link to="/pages/LandingPage">Landing Page</Link>
+        </div>
 
-               {/* <Accordion /> */}
-               <div className='row'>
-                    <div className='col-6'>
-                         <Video />
-                    </div>
-
-                    <div className='col-6'>
-                         <Carousel images={images} />
-                    </div>
-               </div>
-               <div className='row'>
-                    <div className='col-6'>
-                         {" "}
-                         <DragDropBase />
-                    </div>
-
-                    <div className='col-6'>
-                         <DragDrop />
-                    </div>
-               </div>
+        {/* <Accordion /> */}
+        <div className="row">
+          <div className="col-6">
+            <Video />
           </div>
-     </DndProvider>
-);
+
+          <div className="col-6">
+            <Carousel images={images} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            {" "}
+            <DragDropBase />
+          </div>
+
+          <div className="col-6">
+            <DragDrop />
+          </div>
+        </div>
+      </div>
+    </DndProvider>
+  );
+}
 
 export default App;
