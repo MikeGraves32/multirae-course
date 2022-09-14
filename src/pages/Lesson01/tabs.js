045ababcfd739c41a651f4790f81d8791d5b88d6
Mike Graves <mike.graves@l2defense.com>
2022-09-14 03:10:58 -0400

    Update tabs.js

--- src/pages/Lesson01/tabs.js
+++ src/pages/Lesson01/tabs.js
@@ -1,24 +1,19 @@
 import * as React from "react";
+import PropTypes from "prop-types";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
-interface TabPanelProps {
-     children?: React.ReactNode;
-     index: number;
-     value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
+function TabPanel(props) {
      const { children, value, index, ...other } = props;
 
      return (
           <div
                role='tabpanel'
                hidden={value !== index}
-               id={`vertical-tabpanel-${index}`}
-               aria-labelledby={`vertical-tab-${index}`}
+               id={`simple-tabpanel-${index}`}
+               aria-labelledby={`simple-tab-${index}`}
                {...other}
           >
                {value === index && (
@@ -30,45 +25,39 @@ function TabPanel(props: TabPanelProps) {
      );
 }
 
-function a11yProps(index: number) {
+TabPanel.propTypes = {
+     children: PropTypes.node,
+     index: PropTypes.number.isRequired,
+     value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
      return {
-          id: `vertical-tab-${index}`,
-          "aria-controls": `vertical-tabpanel-${index}`,
+          id: `simple-tab-${index}`,
+          "aria-controls": `simple-tabpanel-${index}`,
      };
 }
 
-export default function VerticalTabs() {
+export default function BasicTabs() {
      const [value, setValue] = React.useState(0);
 
-     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+     const handleChange = (event, newValue) => {
           setValue(newValue);
      };
 
      return (
-          <Box
-               sx={{
-                    flexGrow: 1,
-                    bgcolor: "background.paper",
-                    display: "flex",
-                    height: 224,
-               }}
-          >
-               <Tabs
-                    orientation='vertical'
-                    variant='scrollable'
-                    value={value}
-                    onChange={handleChange}
-                    aria-label='Vertical tabs example'
-                    sx={{ borderRight: 1, borderColor: "divider" }}
-               >
-                    <Tab label='Item One' {...a11yProps(0)} />
-                    <Tab label='Item Two' {...a11yProps(1)} />
-                    <Tab label='Item Three' {...a11yProps(2)} />
-                    <Tab label='Item Four' {...a11yProps(3)} />
-                    <Tab label='Item Five' {...a11yProps(4)} />
-                    <Tab label='Item Six' {...a11yProps(5)} />
-                    <Tab label='Item Seven' {...a11yProps(6)} />
-               </Tabs>
+          <Box sx={{ width: "100%" }}>
+               <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                    <Tabs
+                         value={value}
+                         onChange={handleChange}
+                         aria-label='basic tabs example'
+                    >
+                         <Tab label='Item One' {...a11yProps(0)} />
+                         <Tab label='Item Two' {...a11yProps(1)} />
+                         <Tab label='Item Three' {...a11yProps(2)} />
+                    </Tabs>
+               </Box>
                <TabPanel value={value} index={0}>
                     Item One
                </TabPanel>
@@ -77,18 +66,6 @@ export default function VerticalTabs() {
                </TabPanel>
                <TabPanel value={value} index={2}>
                     Item Three
-               </TabPanel>
-               <TabPanel value={value} index={3}>
-                    Item Four
-               </TabPanel>
-               <TabPanel value={value} index={4}>
-                    Item Five
-               </TabPanel>
-               <TabPanel value={value} index={5}>
-                    Item Six
-               </TabPanel>
-               <TabPanel value={value} index={6}>
-                    Item Seven
                </TabPanel>
           </Box>
      );
