ab520f2907bd30f6dad2a2e28f9d14dc96bda6c9
Mike Graves <mike.graves@l2defense.com>
2022-11-15 01:42:52 -0500

    course changes

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,90 +3,100 @@ import React, { useState, useEffect, useRef } from "react";
 import QuizStart from "./QuizStart";
 
 const Question = ({
-  data,
-  onAnswerUpdate,
-  numberOfQuestions,
-  activeQuestion,
-  onSetActiveQuestion,
-  onSetStep,
+     data,
+     onAnswerUpdate,
+     numberOfQuestions,
+     activeQuestion,
+     onSetActiveQuestion,
+     onSetStep,
 }) => {
-  const [selected, setSelected] = useState("");
-  const [error, setError] = useState("");
-  const radiosWrapper = useRef();
+     const [selected, setSelected] = useState("");
+     const [error, setError] = useState("");
+     const radiosWrapper = useRef();
 
-  useEffect(() => {
-    const findCheckedInput =
-      radiosWrapper.current.querySelector("input:checked");
-    if (findCheckedInput) {
-      findCheckedInput.checked = false;
-    }
-  }, [data]);
+     useEffect(() => {
+          const findCheckedInput =
+               radiosWrapper.current.querySelector("input:checked");
+          if (findCheckedInput) {
+               findCheckedInput.checked = false;
+          }
+     }, [data]);
 
-  const changeHandler = (e) => {
-    setSelected(e.target.value);
-    if (error) {
-      setError("");
-    }
-  };
+     const changeHandler = (e) => {
+          setSelected(e.target.value);
+          if (error) {
+               setError("");
+          }
+     };
 
-  const nextClickHandler = (e) => {
-    if (selected === "") {
-      return setError("Please select an option!");
-    }
-    onAnswerUpdate((prevState) => [
-      ...prevState,
-      { q: data.question, a: selected },
-    ]);
-    setSelected("");
-    if (activeQuestion < numberOfQuestions - 1) {
-      onSetActiveQuestion(activeQuestion + 1);
-    } else {
-      onSetStep(3);
-    }
-  };
+     const nextClickHandler = (e) => {
+          if (selected === "") {
+               return setError("Please select an option!");
+          }
+          onAnswerUpdate((prevState) => [
+               ...prevState,
+               { q: data.question, a: selected },
+          ]);
+          setSelected("");
+          if (activeQuestion < numberOfQuestions - 1) {
+               onSetActiveQuestion(activeQuestion + 1);
+          } else {
+               onSetStep(3);
+          }
+     };
 
-  return (
-    <div className="card">
-      <div className="card-content">
-        <div className="content">
-          <div className="row">
-            <div className="col-6">
-              <h3 className="questionHeader">
-                {data.id}. {data.question}
-              </h3>
-            </div>
-            <div className="col-6">
-              <div className="control" ref={radiosWrapper}>
-                {data.choices.map((choice, i) => (
-                  <>
-                    <div>
-                      <img src={data.images[i]} alt="" />
+     return (
+          <div className='question-card'>
+               <div className='question-card-content'>
+                    <div className='content'>
+                         <div className='row'>
+                              <div className='col-6'>
+                                   <h3 className='questionHeader'>
+                                        {data.id}. {data.question}
+                                   </h3>
+                              </div>
+                              <div className='col-6'>
+                                   <div className='control' ref={radiosWrapper}>
+                                        {data.choices.map((choice, i) => (
+                                             <>
+                                                  <div>
+                                                       <img
+                                                            src={data.images[i]}
+                                                            alt=''
+                                                       />
+                                                  </div>
+                                                  <label className='radio has-background-light'>
+                                                       <input
+                                                            type='radio'
+                                                            name='answer'
+                                                            value={choice}
+                                                            aria-label='Question Option'
+                                                            onChange={
+                                                                 changeHandler
+                                                            }
+                                                       />
+                                                       {choice}
+                                                  </label>
+                                             </>
+                                        ))}
+                                   </div>
+                              </div>
+                              {error && (
+                                   <div className='has-text-danger'>
+                                        {error}
+                                   </div>
+                              )}
+                         </div>
+                         <button
+                              className='button is-link is-medium mt-4'
+                              onClick={nextClickHandler}
+                         >
+                              Next Question
+                         </button>
                     </div>
-                    <label className="radio has-background-light">
-                      <input
-                        type="radio"
-                        name="answer"
-                        value={choice}
-                        aria-label="Question Option"
-                        onChange={changeHandler}
-                      />
-                      {choice}
-                    </label>
-                  </>
-                ))}
-              </div>
-            </div>
-            {error && <div className="has-text-danger">{error}</div>}
+               </div>
           </div>
-          <button
-            className="button is-link is-medium mt-4"
-            onClick={nextClickHandler}>
-            Next Question
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+     );
 };
 
 export default Question;
